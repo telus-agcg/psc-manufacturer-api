1c9b4c12fe07ecb5828499787b9af7435a72e890
Kelsey Reid <kelsey.reid@tkxs.com>
2023-05-18 09:56:00 -0500

    fix validation

--- PSC.Manufacturer.API.Core/Entities/Manufacturer.cs
+++ PSC.Manufacturer.API.Core/Entities/Manufacturer.cs
@@ -7,62 +7,50 @@ namespace PSC.Manufacturer.API.Core.Entities
         [Key]
         public int Mfg_Key { get; set; }
 
-        [Required(ErrorMessage = "Mfg_RAPID_ICC is required")]
         [MaxLength(13, ErrorMessage = "Mfg_RAPID_ICC cannot be longer than 13 characters")]
-        public string Mfg_RAPID_ICC { get; set; }
+        public string Mfg_RAPID_ICC { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Mfg_Name is required")]
         [MaxLength(50, ErrorMessage = "Mfg_Name cannot be longer than 50 characters")]
         public string Mfg_Name { get; set; }
 
-        [Required(ErrorMessage = "Mfg_Name_2 is required")]
         [MaxLength(50, ErrorMessage = "Mfg_Name_2 cannot be longer than 50 characters")]
-        public string Mfg_Name_2 { get; set; }
+        public string Mfg_Name_2 { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Address_1 is required")]
         [MaxLength(30, ErrorMessage = "Address_1 cannot be longer than 30 characters")]
-        public string Address_1 { get; set; }
+        public string Address_1 { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Address_2 is required")]
         [MaxLength(30, ErrorMessage = "Address_2 cannot be longer than 30 characters")]
-        public string Address_2 { get; set; }
+        public string Address_2 { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "City is required")]
         [MaxLength(50, ErrorMessage = "City cannot be longer than 50 characters")]
-        public string City { get; set; }
+        public string City { get; set; } = string.Empty;
 
-        [MaxLength(2, ErrorMessage ="State_Code cannot be longer than 50 characters")]
+        [MaxLength(2, ErrorMessage = "State_Code cannot be longer than 50 characters")]
         public string? State_Code { get; set; }
 
-        [Required(ErrorMessage = "Zip_Code is required")]
         [MaxLength(10, ErrorMessage = "Zip_Code cannot be longer than 10 characters")]
-        public string Zip_Code { get; set; }
+        public string Zip_Code { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Edi_Contact is required")]
         [MaxLength(30, ErrorMessage = "City cannot be longer than 30 characters")]
-        public string Edi_Contact { get; set; }
+        public string Edi_Contact { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Phone is required")]
         [MaxLength(12, ErrorMessage = "Phone cannot be longer than 12 characters")]
-        public string Phone { get; set; }
+        public string Phone { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Fax is required")]
         [MaxLength(12, ErrorMessage = "Fax cannot be longer than 12 characters")]
-        public string Fax { get; set; }
+        public string Fax { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "EMail_Address is required")]
         [MaxLength(100, ErrorMessage = "EMail_Address cannot be longer than 100 characters")]
-        [EmailAddress]
-        public string EMail_Address { get; set; }
+        public string EMail_Address { get; set; } = string.Empty;
 
         public DateTime Inserted_Date_Time { get; set; }
 
         [Required(ErrorMessage = "Vendor_Key is required")]
         public int Vendor_Key { get; set; }
-        
+
         public int? Old_Mfg_Key { get; set; }
 
-        [Required(ErrorMessage = "Include_In_Dashboard_Filter is required")]
-        public bool Include_In_Dashboard_Filter { get; set; }
+        public bool Include_In_Dashboard_Filter { get; set; } = false;
     }
 }
