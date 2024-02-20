798836601ba891da36ec89213c1a88356251c687
ivan-obzherin <130067172+ivan-obzherin@users.noreply.github.com>
2024-02-20 18:06:16 +0300

    AB#69181: Healthcheck

---
+++ PSC.Manufacturer.API.Core/HealthcheckTracker.cs
@@ -0,0 +1,33 @@
+﻿using PSC.Manufacturer.API.Core.Dtos;
+
+namespace PSC.Manufacturer.API.Core
+{
+    public class HealthcheckTracker
+    {
+        public async Task<HealthcheckReportDto> Track(string name, Func<Task> healthcheck)
+        {
+            var date = DateTime.UtcNow;
+            var time = 0d;
+            var result = "Ok";
+            try
+            {
+                await healthcheck();
+            }
+            catch (Exception ex)
+            {
+                result = ex.ToString();
+            }
+            finally
+            {
+                time = DateTime.UtcNow.Subtract(date).TotalMilliseconds;
+            }
+
+            return new HealthcheckReportDto
+            {
+                Description = name,
+                Status = result,
+                ResponseTimeMs = time,
+            };
+        }
+    }
+}
--- PSC.Manufacturer.API.DataAccess/ManufacturerApiDbContext.cs
+++ PSC.Manufacturer.API.DataAccess/ManufacturerApiDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using PSC.Manufacturer.API.Core.Entities;
 
 namespace PSC.Manufacturer.API.DataAccess
@@ -29,9 +30,22 @@ namespace PSC.Manufacturer.API.DataAccess
             modelBuilder.Entity<Vendor>()
                 .ToTable("Vendor")
                 .HasKey(x => x.Vendor_Key);
+
+            modelBuilder.Entity<ApiLog>(entity =>
+            {
+                entity.ToTable("Api_Log", "mdmrecon");
+                entity.HasKey(x => x.ApiLogKey).HasName("PK_Mdmrecon_Api_Log");
+
+                entity.Property(x => x.ApiLogKey)
+                    .ValueGeneratedOnAdd()
+                    .UseIdentityColumn()
+                    .HasColumnType("bigint")
+                    .HasColumnName("Api_Log_Key");
+            });
         }
 
         public DbSet<Core.Entities.Manufacturer> Manufacturers { get; set; }
         public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<ApiLog> ApiLogs { get; set; }
     }
 }
